e1143b58ffdfece3f43cbc0495e1de517ba18c22
jarl.ostensen@gmail.com <jarl.ostensen@gmail.com>
2020-03-08 17:51:58 +0000

    single loop for both 1f and b leafs.

--- main.cpp
+++ main.cpp
@@ -100,67 +100,77 @@ namespace perf
 		
 		constexpr unsigned kSubLeaf_SMTLevel = 0;
 		constexpr unsigned kSubLeaf_ProcessorCore = 1;
-			
-		if(_proc_info._cpuid_caps._1f_leaf)
+		const auto leaf = (_proc_info._cpuid_caps._1f_leaf ? 0x1f : 0xb);
+		
+		// https://github.com/open-mpi/hwloc/blob/master/hwloc/topology-x86.c
+
+		std::cout << std::hex << leaf << " leaf\n";
+		auto level_type = 0u;
+		auto level_shift = 0u;
+		auto count = 0u;
+		_proc_info._phys_cores = 0;
+		_proc_info._num_cores = 0;
+
+		// determine max level first (package)
+		auto package_shift = 0u;
+		for(auto sub_leaf = kSubLeaf_SMTLevel; ;++sub_leaf)
 		{
-			//TODO: this requires work, it's not correct
-		    std::cout << "1f leaf\n";
-			cpu_id = {0x1f,kSubLeaf_SMTLevel};
-			std::cout << "leaf 0 " << cpu_id << "\n";
-			std::cout << cpu_id.extract_reg_field(cpuid::regs::ecx,0,7) << "\n";
-
-			_proc_info._smt_mask_width = cpu_id.extract_reg_field(cpuid::regs::eax, 0, 4);
-			//NOTE: IA Dev manual states this number isn't reliable
-			const auto num_smt_cores = cpu_id.extract_reg_field(cpuid::regs::ebx, 0,15);
-			cpu_id = {0x1f,kSubLeaf_ProcessorCore};
-			std::cout << "leaf 1 " << cpu_id << "\n";
-			_proc_info._core_mask_width = cpu_id.extract_reg_field(cpuid::regs::eax, 0, 4);			
-
-		}
-		else if ( _proc_info._cpuid_caps._b_leaf )
-		{			
-			std::cout << "b leaf\n";
-			auto level_type = 0u;
-			auto level_shift = 0u;
-			_proc_info._phys_cores = 0;
-			_proc_info._num_cores = 0;
-			for(auto sub_leaf = kSubLeaf_SMTLevel; ;++sub_leaf)
+			cpu_id = {leaf,sub_leaf};
+			if(!cpu_id.ebx() && !cpu_id.eax())
 			{
-				cpu_id = {0xb,sub_leaf};
-				if(!cpu_id.ebx())
+				// done 
+				break;
+			}
+			package_shift = cpu_id.extract_reg_field(cpuid::regs::eax, 0, 4);
+		}
+
+		std::cout << "package shift is " << package_shift << "\n";
+
+		// now iterate levels to detemine topology
+		for(auto sub_leaf = kSubLeaf_SMTLevel; ;++sub_leaf)
+		{
+			cpu_id = {leaf,sub_leaf};
+			if(!cpu_id.ebx() && !cpu_id.eax())
+			{
+				// done 
+				break;
+			}
+			level_type = cpu_id.extract_reg_field(cpuid::regs::ecx, 8, 15);
+			level_shift = cpu_id.extract_reg_field(cpuid::regs::eax, 0, 4);
+			count = cpu_id.extract_reg_field(cpuid::regs::ebx, 0, 15);
+			const auto id = (cpu_id.edx() >> level_shift) & ((1u << (package_shift - level_shift)) - 1u);
+			std::cout << std::hex << "x2APIC id 0x" << id << ", number " << count << "\n";
+			switch(level_type)
+			{
+				case 1:
 				{
-					// done 
-					break;
+					// SMT
+					_proc_info._smt_mask_width = level_shift;
+					//std::cout << "SMT " << level_shift << ", 2xAPIC id " << cpu_id.edx() << "\n";
 				}
-				level_type = cpu_id.extract_reg_field(cpuid::regs::ecx, 8, 15);
-				level_shift = cpu_id.extract_reg_field(cpuid::regs::eax, 0, 4);
-				switch(level_type)
+				break;
+				case 2:
 				{
-					case 1:
-					{
-						// SMT
-						_proc_info._smt_mask_width = level_shift;
-						//std::cout << "SMT " << level_shift << ", 2xAPIC id " << cpu_id.edx() << "\n";
-					}
-					break;
-					case 2:
-					{
-						// core
-						//NOTE: this level shift includes the SMT shift (see Intel docs)
-						_proc_info._core_mask_width = level_shift;
-						// remove SMT part, this is the number of physical cores available in this package, but it's not guaranteed to be the actual count present...
-						_proc_info._phys_cores += 1u << size_t(level_shift >> _proc_info._smt_mask_width);
-						_proc_info._num_cores += cpu_id.ebx();
-						//std::cout << "core " << level_shift << ", 2xAPIC id " << cpu_id.edx() << "\n";
-					}
-					break;
-					default:
-						std::cout << "Extra: type " << level_type << ", level shift " << level_shift << "\n";
-					break;
+					// core
+					//NOTE: this level shift includes the SMT shift (see Intel docs)
+					_proc_info._core_mask_width = level_shift;
+					// remove SMT part, this is the number of physical cores available in this package, but it's not guaranteed to be the actual count present...
+					_proc_info._phys_cores += 1u << size_t(level_shift >> _proc_info._smt_mask_width);
+					_proc_info._num_cores += count;
+					//std::cout << "core " << level_shift << ", 2xAPIC id " << cpu_id.edx() << "\n";
 				}
+				break;
+				case 3:
+				{
+					std::cout << "package\n"; 
+				}
+				break;
+				default:
+					std::cout << "Unhandled type " << level_type << ", level shift " << level_shift << "\n";
+				break;
 			}
 		}
-
+		
 		std::cout << "phys cores (processors) " << _proc_info._phys_cores << ", logical cores " << _proc_info._num_cores << "\n";
 	}
 
